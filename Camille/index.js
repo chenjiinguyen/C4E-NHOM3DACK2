03fcf1c8d50b0bebb1df4c9193617e7483497b9a
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-01-28 23:31:59 +0700

    Update index.js

--- Camille/index.js
+++ Camille/index.js
@@ -1,19 +1,20 @@
  const usersElement = document.querySelector('.champions');
   usersElement.innerHTML = "";
   let htmlData = "";
-  
-      for (const item of champions) {
-          let html = `
-          
-  <ul class="champions" id="champions">
-  <li >
-      <img class="image" src="${item.image}" height="120" alt="">
-      <div class="name">${item.name}</div>
-      <li >
-  </ul>`;
-  
-          htmlData += html;
-      
-  }
-  
-  usersElement.innerHTML = htmlData;
+
+
+  for (const item of champions) {
+    let html = `
+    
+<ul class="champions" id="champions">
+<li >
+<img class="image" src="${item.image}" height="120" alt="">
+<div class="name">${item.name}</div>
+</li>
+</ul>`
+
+    htmlData += html;
+
+}
+
+usersElement.innerHTML = htmlData;
