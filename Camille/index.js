858a06d0c7f116c530b1754c1811b9e0abe5950c
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-01-28 16:12:44 +0700

    Create index.js

---
+++ Camille/index.js
@@ -0,0 +1,19 @@
+ const usersElement = document.querySelector('.champions');
+  usersElement.innerHTML = "";
+  let htmlData = "";
+  
+      for (const item of champions) {
+          let html = `
+          
+  <ul class="champions" id="champions">
+  <li >
+      <img class="image" src="${item.image}" height="120" alt="">
+      <div class="name">${item.name}</div>
+      <li >
+  </ul>`;
+  
+          htmlData += html;
+      
+  }
+  
+  usersElement.innerHTML = htmlData;
