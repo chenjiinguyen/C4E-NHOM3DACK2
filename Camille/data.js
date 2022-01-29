aed3291702a412f259a0e054111d1ef93555ffc9
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-01-29 22:19:23 +0700

    Update data.js

--- Camille/data.js
+++ Camille/data.js
@@ -1,8 +1,8 @@
 const champions = [
-    {
-      id: 1,
-      name: "Camille",
-      image: "https://kicdo.com/images/source/champion/Camille.png",
-      lane: ["top"],
-    },
+  {
+    id: 1,
+    name: "Camille",
+    image: "https://kicdo.com/images/source/champion/Camille.png",
+    lane: ["top"],
+  },
 ];
