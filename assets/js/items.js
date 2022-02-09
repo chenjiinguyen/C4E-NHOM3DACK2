b45d8ff43b9a838f7f1be5a82d4609b221b217b2
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-02-09 14:59:03 +0700

    Update items.js

--- assets/js/items.js
+++ assets/js/items.js
@@ -27,35 +27,37 @@ if(params.type){
     for (const item of items) {
         if (item.type.includes(params.type)) {
                 let html = `
-                
-                <ul class="items">
                 <li class="dropdownA">
                     <img class="image" src="${item.image}" height="80" alt="">
                     <p class="name">${item.name}</p>
                     <div class="dropdown-contentB">
                         <a class="description">${item.description}</a>
                     </div>
-                </li>
-                </ul>`;
-                htmlData += html;
-            
+                </li>`;
+                htmlData += html;          
         }
     }
     
 }else{
     for (const item of items) {
                 let html = `
-                
-                <ul class="items">
                 <li class="dropdownA">
                     <img class="image" src="${item.image}" height="80" alt="">
                     <p class="name">${item.name}</p>
                     <div class="dropdown-contentB">
                         <a class="description">${item.description}</a>
                     </div>
-                </li>
-                </ul>`;
+                </li>`;
                 htmlData += html;
     }
 }
 usersElement.innerHTML = htmlData;
+
+domItems = document.querySelectorAll(".dropdownA");
+for (let i = 0; i < Math.floor(domItems.length/7); i++) {
+    for (let k = 0; k < 8; k++) {
+        if (k >= 4 && k <= 7){
+            domItems[8*i+k].querySelector(".dropdown-contentB").classList.add("right");
+        }
+    }
+}
