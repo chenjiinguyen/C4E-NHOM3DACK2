4086bff1b7696401f00a0c822fe686a6b90d49e7
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-02-15 21:11:09 +0700

    all

---
+++ assets/js/image.js
@@ -0,0 +1,64 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction5() {
+    document.getElementById("myDropdown5").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction6() {
+    document.getElementById("myDropdown6").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown7").classList.add("dropdown-image");
+  }
+  function myFunction7() {
+    document.getElementById("myDropdown7").classList.toggle("dropdown-image");
+    document.getElementById("myDropdown").classList.add("dropdown-image");
+    document.getElementById("myDropdown2").classList.add("dropdown-image");
+    document.getElementById("myDropdown3").classList.add("dropdown-image");
+    document.getElementById("myDropdown4").classList.add("dropdown-image");
+    document.getElementById("myDropdown5").classList.add("dropdown-image");
+    document.getElementById("myDropdown6").classList.add("dropdown-image");
+  }
+  
