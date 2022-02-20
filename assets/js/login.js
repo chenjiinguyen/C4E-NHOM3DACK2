8535399a291d63456707944f34061a281a10b7fe
DangTuan03 <tuandmhe170748@fpt.edu.vn>
2022-02-20 20:22:19 +0700

    all

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,38 @@
+document.getElementById("true").style.display = "none"
 function verify(){
     var username = document.getElementById("username").value
     var atposition = username.indexOf("@")
     var dotposition = username.lastIndexOf(".")
     var matkhau = document.getElementById("matkhau").value
     if (atposition < 1 || dotposition < atposition+2 || username.length <= dotposition+2){
-    document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng. Truy cập vào trang không thể đăng nhập để biết thêm thông tin."
+    document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng."
     document.getElementById("error").style.color = "purple";
+    document.getElementById("username").style.background = "pink";
+    document.getElementById("matkhau").style.background = "pink";
     return false
     }
     else{
       if(matkhau ==""){
-      document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng. Truy cập vào trang không thể đăng nhập để biết thêm thông tin."
+      document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng."
       document.getElementById("error").style.color = "purple";
+      document.getElementById("username").style.background = "pink";
+      document.getElementById("matkhau").style.background = "pink";
       return false
     }
       if(matkhau.length<8){
-      document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng. Truy cập vào trang không thể đăng nhập để biết thêm thông tin."
+      document.getElementById("error").innerHTML = "Có thể tên người dùng hoặc mật khẩu của bạn không chính xác, hoặc có thể bạn cần cập nhật Tài khoản Riot của mình nếu bạn đã ngừng chơi trong vài tháng."
       document.getElementById("error").style.color = "purple";
+      document.getElementById("username").style.background = "pink";
+      document.getElementById("matkhau").style.background = "pink";
       return false
     }
       else{
-      document.getElementById("error").style.color = "DodgerBlue";
+        document.getElementById("error").innerHTML = ""
+        document.getElementById("username").style.background = "aquamarine";
+        document.getElementById("matkhau").style.background = "aquamarine";
+        window.location.replace("/index.html")
+        return false
     }
     }
     }
+
